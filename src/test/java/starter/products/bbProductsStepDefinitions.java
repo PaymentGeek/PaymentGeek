b4495ed0db90e44285de5a077c660e0725f03e55
PaymentGeek <dan.bulacu@gmail.com>
2021-06-25 18:22:46 +0200

    XRAY-0006 Added response body check and a negative scenario

--- src/test/java/starter/products/bbProductsStepDefinitions.java
+++ src/test/java/starter/products/bbProductsStepDefinitions.java
@@ -2,13 +2,21 @@ package starter.products;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ResponseBody;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
+import org.junit.Assert;
 import starter.postcodes.LocationResponse;
 
+import java.util.List;
+
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class bbProductsStepDefinitions {
 
@@ -28,5 +36,14 @@ public class bbProductsStepDefinitions {
     @Then("the response code should be {}")
     public void theResultIsValid(int responseCode) {
         restAssuredThat(response -> response.statusCode(responseCode));
+    }
+
+    @Then("the response body field {} is not empty")
+    public void theResultIsNotEmpty(String field) {
+        ResponseBody body = SerenityRest.lastResponse();
+        JsonPath json = body.jsonPath();
+
+        Integer jsonField = json.get(field);
+        Assert.assertTrue(jsonField != null );
     }
 }
--- src/test/resources/features/bestBuy/best_buy_products.feature
+++ src/test/resources/features/bestBuy/best_buy_products.feature
@@ -3,12 +3,25 @@ Feature: Best Buy Products Feature
   Scenario: I get a list of products from Best Buy
     When I get all products from the store
     Then the response code should be 200
+    And the response body field 'total' is not empty
 
   Scenario Outline: I get a product based on an ID
     When I get a product by id <id>
     Then the response code should be <response_code>
+    And the response body field <field> is not empty
+
     Examples:
-    |id          |response_code |
-    |43900       |200           |
-    |48530       |200           |
-    |127687      |200           |
+    |id          |response_code |field |
+    |43900       |200           |id    |
+    |48530       |200           |id    |
+    |127687      |200           |id    |
+
+  Scenario Outline: I get a product based on an invalid ID
+    When I get a product by id <id>
+    Then the response code should be <response_code>
+
+    Examples:
+      |id          |response_code |
+      |122345      |404           |
+      |x           |404           |
+      |0           |404           |
